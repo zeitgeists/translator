acce07ff541cd1ff22b53b4bb719bceebf998323
FlightShkaff <Flight0208@gmail.com>
2023-05-20 20:55:43 +0300

    write graph in file

--- include/STD.hpp
+++ include/STD.hpp
@@ -5,5 +5,6 @@
 #endif
 
 #include <iostream>
+#include <fstream>
 
 extern "C" DLLEXPORT double graph(double min, double max, double step, double n);
--- src/STD.cpp
+++ src/STD.cpp
@@ -2,13 +2,16 @@
 #include "AST.hpp"
 
 extern "C" DLLEXPORT double graph(double min, double max, double step, double n) {
-    std::string functionName =  std::format("{}{}", "func", n);
+    //std::string functionName =  std::format("{}{}", "func", n);
+    std::string functionNum = std::to_string(n);
+    std::string functionName = "func" + std::to_string(n);
 
     auto Result = TheJIT->lookup(functionName);
     if (auto E = Result.takeError()) {
         Logger::LogError(
-            std::format("function 'graph' called but {} function was not found",
-            functionName));
+           // std::format("function 'graph' called but {} function was not found",
+           // functionName)
+           "function 'graph' called but " + functionName + " function was not found");
         return -1;
     }
     double (*FP)(double) =
@@ -16,8 +19,16 @@ extern "C" DLLEXPORT double graph(double min, double max, double step, double n)
     if (!FP) {
     }
 
-    for (double i = min; i < max; i += step) {
-        std::cout << i << "," << FP(i) << std::endl;
+    std::ofstream outputFile("data.txt");
+
+    if (outputFile.is_open()) {
+        for (double i = min; i < max; i += step) {
+            outputFile << i << "," << FP(i) << std::endl;
+        }
+        outputFile.close();
+    } else {
+    
+    std::cout << "Unable to open the file." << std::endl;
     }
     return 0;
 }
