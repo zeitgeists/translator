c9be62dd921a874810dce1184bb8fb7f48e85c60
zeitgeists <ewggen3@gmail.com>
2023-06-03 14:22:10 +0300

    feat: Parser class with FSM

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -5,28 +5,34 @@
 #include "Logger.hpp"
 #include "Token.hpp"
 #include "Lexer.hpp"
-#include "AST.hpp"
+#include "CodeGenerator.hpp"
+// #include "AST.hpp"
 
-static Token currentToken;
+class Parser {
+public:
+    explicit Parser(std::unique_ptr<Lexer>);
+    void ParseLoop();
+private:
+    class FSM {
+    private:
+        struct State {
+            std::list<Token> expectedTokens = {};
+            int next = 0;
+            bool isReadNextToken = false;
+            int toStack = 0;
+            bool pushTerm = false;
+            bool pushOperator = false;
+            int error = -1; // if -1 then error can NOT be ignored;
+            void (*CG)() = nullptr; // CodeGen
+        };
+    public:
+        FSM();
+        ~FSM();
+        State* states;
+    };
 
-namespace Parser {
-    void GetNextToken();
-    std::unique_ptr<ExprAST> ParseTerm();
-    std::unique_ptr<ExprAST> ParseNegation();
-    std::unique_ptr<ExprAST> ParseOP_2_List();
-    std::unique_ptr<ExprAST> ParseOP_2_ListTail(std::unique_ptr<ExprAST> LHS);
-    std::unique_ptr<ExprAST> ParseOP_1_List();
-    std::unique_ptr<ExprAST> ParseOP_1_ListTail(std::unique_ptr<ExprAST> LHS);
-    std::unique_ptr<ExprAST> ParseExpression();
-    std::unique_ptr<std::vector<std::string>> ParseParamList();
-    std::unique_ptr<std::vector<std::string>> ParseParamListTail(
-            std::unique_ptr<std::vector<std::string>> Params);
-    std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> ParseArgList();
-    std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> ParseArgListTail(
-            std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> Args);
-    std::unique_ptr<PrototypeAST> ParsePrototype();
-    std::unique_ptr<PrototypeAST> ParseExtern();
-    std::unique_ptr<FunctionAST> ParseDefinition();
-    std::unique_ptr<FunctionAST> ParseTopLevelExpr();
-    Token GetCurrentToken();
-}
+    std::unique_ptr<Lexer> lexer;
+    Token currentToken;
+    static const FSM fsm;
+    CodeGenerator codeGen;
+};
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,195 +1,73 @@
 #include "Parser.hpp"
 #include <memory>
-#include <string>
 
-Token Parser::GetCurrentToken() {
-    return currentToken;
+Parser::Parser(std::unique_ptr<Lexer> l) : lexer(std::move(l)) {}
+
+void Parser::ParseLoop() {
+    lexer->GetToken();
 }
 
-void Parser::GetNextToken() {
-    currentToken = Lexer::GetToken();
-}
+Parser::FSM::FSM() {
+    states = new State[57];
 
-std::unique_ptr<ExprAST> Parser::ParseTerm() {
-    if (currentToken.tokenId == tok_number) {
-        auto result = std::make_unique<NumberExprAST>(currentToken.numVal);
-        GetNextToken(); // consume the number
-        return result;
+    states[ 0] = { {},  0, false,  0, false, false,  -1, nullptr };
+    states[ 1] = { {}, -1, false,  0, false, false,  -1, nullptr };
+    states[ 2] = { {},  4, false,  3, false, false,  -1, nullptr };
+    states[ 3] = { {},  0, false,  0, false, false,  -1, nullptr };
+    states[ 4] = { {}, 12, false,  5, false, false,  -1, nullptr };
+    states[ 5] = { {},  6,  true,  0, false, false,  -1, nullptr };
+    states[ 6] = { {},  7, false,  0, false, false,  -1, nullptr };
+    states[ 7] = { {},  4, false,  0, false, false,  -1, nullptr };
+    states[ 8] = { {}, 11, false,  0, false, false,  -1, nullptr };
+    states[ 9] = { {}, 13, false,  0, false, false,  -1, nullptr };
+    states[10] = { {}, 25, false,  0, false, false,  -1, nullptr };
+    states[11] = { {}, 12,  true,  0, false, false,  -1, nullptr };
+    states[12] = { {}, 16, false,  0, false, false,  -1, nullptr };
+    states[13] = { {}, 14,  true,  0, false, false,  -1, nullptr };
+    states[14] = { {}, 16, false,  0, false, false,  -1, nullptr };
+    states[15] = { {}, 25, false,  0, false, false,  -1, nullptr };
+    states[16] = { {}, 17,  true,  0, false, false,  -1, nullptr };
+    states[17] = { {}, 18,  true,  0, false, false,  -1, nullptr };
+    states[18] = { {}, 20, false,  0, false, false,  -1, nullptr };
+    states[19] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[20] = { {}, 21,  true,  0, false, false,  -1, nullptr };
+    states[21] = { {}, 22, false,  0, false, false,  -1, nullptr };
+    states[22] = { {}, 23,  true,  0, false, false,  -1, nullptr };
+    states[23] = { {}, 24,  true,  0, false, false,  -1, nullptr };
+    states[24] = { {}, 24, false,  0, false, false,  -1, nullptr };
+    states[25] = { {}, 26, false,  0, false, false,  -1, nullptr };
+    states[26] = { {}, 31, false,  0, false, false,  -1, nullptr };
+    states[27] = { {}, 28, false,  0, false, false,  -1, nullptr };
+    states[28] = { {}, 45, false,  0, false, false,  -1, nullptr };
+    states[29] = { {}, 31, false,  0, false, false,  -1, nullptr };
+    states[30] = { {}, 28, false,  0, false, false,  -1, nullptr };
+    states[31] = { {}, 36, false,  0, false, false,  -1, nullptr };
+    states[32] = { {}, 33, false,  0, false, false,  -1, nullptr };
+    states[33] = { {}, 47, false,  0, false, false,  -1, nullptr };
+    states[34] = { {}, 36, false,  0, false, false,  -1, nullptr };
+    states[35] = { {}, 33, false,  0, false, false,  -1, nullptr };
+    states[36] = { {}, 39, false,  0, false, false,  -1, nullptr };
+    states[37] = { {}, 38,  true,  0, false, false,  -1, nullptr };
+    states[38] = { {}, 39, false,  0, false, false,  -1, nullptr };
+    states[39] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[40] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[41] = { {}, 42,  true,  0, false, false,  -1, nullptr };
+    states[42] = { {}, 25, false,  0, false, false,  -1, nullptr };
+    states[43] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[44] = { {}, 49, false,  0, false, false,  -1, nullptr };
+    states[45] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[46] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[47] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[48] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[49] = { {}, 50,  true,  0, false, false,  -1, nullptr };
+    states[50] = { {}, 51,  true,  0, false, false,  -1, nullptr };
+    states[51] = { {}, 53, false,  0, false, false,  -1, nullptr };
+    states[52] = { {}, -1,  true,  0, false, false,  -1, nullptr };
+    states[53] = { {}, 25, false,  0, false, false,  -1, nullptr };
+    states[54] = { {}, 55, false,  0, false, false,  -1, nullptr };
+    states[55] = { {}, 56,  true,  0, false, false,  -1, nullptr };
+    states[56] = { {}, 25, false,  0, false, false,  -1, nullptr };
+    states[57] = { {}, 55, false,  0, false, false,  -1, nullptr };
 
-    } else if (currentToken.tokenId == '(') {
-        GetNextToken(); // eat '('
-        auto result = ParseExpression();
-        GetNextToken(); // eat ')'
-        return std::move(result);
-
-    } else if (currentToken.tokenId == tok_identifier) {
-        std::string IdName = currentToken.identifierStr;
-        GetNextToken(); // eat identifier.
-        if (currentToken.tokenId != '(') // Simple variable reference
-            return std::make_unique<VariableExprAST>(IdName);
-
-        // Call.
-        GetNextToken(); // eat '('
-        std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>>
-            Args = ParseArgList();
-        if (!Args) return Logger::LogError("Error while parsing ArgList");
-        GetNextToken(); // eat ')'
-        return std::make_unique<CallExprAST>(IdName, std::move(Args));
-
-    } else {
-        return Logger::LogError("Unknown token when expecting an TERM");
-    }
-}
-
-std::unique_ptr<ExprAST> Parser::ParseNegation() {
-    // TODO: this check is bad, negation must have his own tokenId
-    if (currentToken.tokenId == tok_operator_1 &&
-            currentToken.operatorStr == "-") {
-        // TODO: add negation
-        return Logger::LogError("Negation not implemented yet");
-
-    } else {
-        return ParseTerm();
-    }
-}
-
-std::unique_ptr<ExprAST> Parser::ParseOP_1_List() {
-    auto LHS = ParseOP_2_List();
-    if (!LHS) return nullptr;
-
-    return ParseOP_1_ListTail(std::move(LHS));
-}
-
-std::unique_ptr<ExprAST> Parser::ParseOP_1_ListTail(std::unique_ptr<ExprAST> LHS) {
-    if (currentToken.tokenId != tok_operator_1) return LHS;
-
-    std::string opStr = currentToken.operatorStr;
-    GetNextToken(); //consume operator_1
-    auto RHS = ParseOP_2_List();
-    // Merge LHS/RHS.
-    auto new_LHS = std::make_unique<OperatorAST>(opStr, std::move(LHS), std::move(RHS));
-    return ParseOP_1_ListTail(std::move(new_LHS));
-}
-
-std::unique_ptr<ExprAST> Parser::ParseOP_2_List() {
-    auto LHS = ParseNegation();
-    if (!LHS) return nullptr;
-
-    return ParseOP_2_ListTail(std::move(LHS));
-}
-
-std::unique_ptr<ExprAST> Parser::ParseOP_2_ListTail(std::unique_ptr<ExprAST> LHS) {
-    if (currentToken.tokenId != tok_operator_2) return LHS;
-
-    std::string opStr = currentToken.operatorStr;
-    GetNextToken(); //consume operator_2
-    auto RHS = ParseNegation();
-    // Merge LHS/RHS.
-    auto new_LHS = std::make_unique<OperatorAST>(opStr, std::move(LHS), std::move(RHS));
-    return ParseOP_2_ListTail(std::move(new_LHS));
-}
-
-std::unique_ptr<ExprAST> Parser::ParseExpression() {
-    return ParseOP_1_List();
-}
-
-std::unique_ptr<std::vector<std::string>> Parser::ParseParamList() {
-    auto Params = std::make_unique<std::vector<std::string>>();
-    if (currentToken.tokenId == tok_identifier) {
-        Params->push_back(currentToken.identifierStr);
-        GetNextToken(); // eat identifier.
-        return ParseParamListTail(std::move(Params));
-    } else {
-        return std::move(Params);
-    }
-}
-std::unique_ptr<std::vector<std::string>> Parser::ParseParamListTail(
-            std::unique_ptr<std::vector<std::string>> Params) {
-    if (currentToken.tokenId != ',') return std::move(Params);
-
-    GetNextToken(); // eat ','
-
-    if (currentToken.tokenId == tok_identifier) {
-        Params->push_back(currentToken.identifierStr);
-        GetNextToken(); // eat identifier.
-        return ParseParamListTail(std::move(Params));
-    } else {
-        return nullptr;
-    }
-}
-
-std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> Parser::ParseArgList() {
-    auto Args = std::make_unique<std::vector<std::unique_ptr<ExprAST>>>();
-
-    auto arg = ParseExpression();
-    if (!arg) return std::move(Args);
-
-    Args->push_back(std::move(arg));
-    return ParseArgListTail(std::move(Args));
-}
-
-std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> Parser::ParseArgListTail(
-        std::unique_ptr<std::vector<std::unique_ptr<ExprAST>>> Args) {
-    if (currentToken.tokenId != ',') return std::move(Args);
-
-    GetNextToken(); // eat ','
-
-    if (auto arg = ParseExpression()) {
-        Args->push_back(std::move(arg));
-        return ParseArgListTail(std::move(Args));
-    } else {
-        return nullptr;
-    }
-}
-
-std::unique_ptr<PrototypeAST> Parser::ParsePrototype() {
-    if (currentToken.tokenId != tok_identifier)
-        return Logger::LogErrorP("Expected function name in prototype");
-
-    std::string FnName = currentToken.identifierStr;
-    Parser::GetNextToken();
-
-    if (currentToken.tokenId != '(')
-        return Logger::LogErrorP("Expected '(' in prototype");
-
-    Parser::GetNextToken(); // eat '('
-
-    std::unique_ptr<std::vector<std::string>>
-        Params = ParseParamList();
-    if (!Params) return Logger::LogErrorP("Error while parsing ParamList");
-
-    if (currentToken.tokenId != ')') {
-        Lexer::PrintLoggedTokens();
-        return Logger::LogErrorP("Expected ')' in prototype");
-    }
-
-    // success.
-    Parser::GetNextToken(); // eat ')'.
-
-    return std::make_unique<PrototypeAST>(FnName, std::move(Params));
-}
-
-std::unique_ptr<PrototypeAST> Parser::ParseExtern() {
-    Parser::GetNextToken(); // eat 'extern'
-    return ParsePrototype();
-}
-
-std::unique_ptr<FunctionAST> Parser::ParseDefinition() {
-    Parser::GetNextToken(); // eat def.
-    auto Prototype = ParsePrototype();
-    if (!Prototype) return nullptr;
-
-    if (auto E = Parser::ParseExpression())
-        return std::make_unique<FunctionAST>(std::move(Prototype), std::move(E));
-    return nullptr;
-}
-
-std::unique_ptr<FunctionAST> Parser::ParseTopLevelExpr() {
-    if (auto E = ParseExpression()) {
-        // Make an anonymous proto.
-        auto Proto = std::make_unique<PrototypeAST>("__anonymous_expr",
-                                std::make_unique<std::vector<std::string>>());
-        return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
-    }
-    return nullptr;
+    states[0].expectedTokens.push_back({ Token::Identifier, Token::None });
 }
